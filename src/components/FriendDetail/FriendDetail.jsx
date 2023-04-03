410c0230bbd473ae14add011a4b663a892f81e31
rakib-molla <rakibmail1997@gmail.com>
2023-04-03 14:44:40 +0600

    display user create dynamic link eith router parameter

---
+++ src/components/FriendDetail/FriendDetail.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const FriendDetail = () => {
+    return (
+        <div>
+            <h1>everything </h1>
+        </div>
+    );
+};
+
+export default FriendDetail;
--- src/main.jsx
+++ src/main.jsx
@@ -6,19 +6,51 @@ import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
+import About from './components/About/About';
+import Contact from './components/Contact/Contact';
+import Home from './components/Home/Home';
+import Friends from './components/Friends/Friends';
+import FriendDetail from './components/FriendDetail/FriendDetail';
+
+// const router = createBrowserRouter([
+//   {
+//     path: "/",
+//     element: <App></App>
+//   },
+//   {
+//     path: '/about',
+//     element: <About></About>
+//   },
+//   {
+//     path: '/contact',
+//     element: <Contact></Contact>
+//   }
+// ])
 
 const router = createBrowserRouter([
   {
-    path: "/",
-    element: <App></App>
-  },
-  {
-    path: '/about',
-    element: <div>this is about page</div>
-  },
-  {
-    path: '/contact',
-    element: <div> this is contact page</div>
+    path: '/',
+    element: <Home></Home>,
+    children:[
+      
+      {
+        path: 'about',
+        element: <About></About>
+      },
+      {
+        path: 'friends',
+        element: <Friends></Friends>,
+        loader: ()=> fetch('https://jsonplaceholder.typicode.com/users')
+      },
+      {
+        path: 'friend/:friendId',
+        element: <FriendDetail> </FriendDetail>
+      },
+      {
+        path: 'contact',
+        element: <Contact></Contact>
+      }
+    ]
   }
 ])
 
